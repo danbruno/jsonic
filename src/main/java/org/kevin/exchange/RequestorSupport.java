002a60d5e1f88d1a677a13c43d26052342f94b35
danbruno <dan@danbruno.ca>
2020-01-02 11:57:34 -0500

    Add logging

--- src/main/java/org/kevin/exchange/RequestorSupport.java
+++ src/main/java/org/kevin/exchange/RequestorSupport.java
@@ -2,15 +2,18 @@ package org.kevin.exchange;
 
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.Channel;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+@Slf4j
 public abstract class RequestorSupport implements Requestor {
 
     @Override
     public void request(Channel channel) {
         List<Object> requests = writeRequests(channel.alloc());
         requests.forEach(channel::write);
+        log.info("Wrote requests");
         channel.flush();
     }
 
